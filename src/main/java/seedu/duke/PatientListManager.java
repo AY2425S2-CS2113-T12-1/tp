c6de03cef7abed5ae77932aac323c7f8159ebba0
siqiangz <szhongq21@gmail.com>
2025-03-19 00:17:44 +0800

    Added PatientListManager, PatientLister without full implementation

---
+++ src/main/java/seedu/duke/PatientListManager.java
@@ -0,0 +1,11 @@
+package seedu.duke;
+
+import java.util.ArrayList;
+
+public class PatientListManager {
+    private static ArrayList<Patient> patientList = new ArrayList<>();
+
+    public static ArrayList<Patient> getPatientList() {
+        return patientList;
+    }
+}
--- src/main/java/seedu/duke/PatientLister.java
+++ src/main/java/seedu/duke/PatientLister.java
@@ -1,7 +1,9 @@
 package seedu.duke;
 
+import java.util.ArrayList;
+
 public class PatientLister {
     public static void listPatients() {
-
+        ArrayList<Patient> patientList = PatientListManager.getPatientList();
     }
 }
