054d86481cff5e2f229cbf33a52b19e703b3a90f
cj143n <chuaje99@gmail.com>
2025-03-18 23:57:58 +0800

    Create Patient and Doctor classes

---
+++ src/main/java/seedu/duke/Doctor.java
@@ -0,0 +1,32 @@
+package seedu.duke;
+
+public class Doctor {
+    private String name;
+    private String specialisation;
+    private String availability;
+    private String patientsBeingTreated;
+
+    public Doctor(String name, String specialisation, String availability, String patientsBeingTreated) {
+        this.name = name;
+        this.specialisation = specialisation;
+        this.availability = availability;
+        this.patientsBeingTreated = patientsBeingTreated;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialisation() {
+        return specialisation;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public String getPatientsBeingTreated() {
+        return patientsBeingTreated;
+    }
+
+}
