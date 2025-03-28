ef81d0ef2befb49c1e4ce346678ebc34faa5d39b
jhu0748 <jackson.j.hu@gmail.com>
2025-03-28 20:33:50 +0800

    View most frequent doctor type/specialisation

--- src/main/java/seedu/medinote/manager/TaskManager.java
+++ src/main/java/seedu/medinote/manager/TaskManager.java
@@ -63,7 +63,7 @@ public class TaskManager {
             if(arguments.equals("doctor type")) {
                 ViewDoctorFrequencies.viewMostFrequentSpecialisation();
             } else if(arguments.equals("visited doctor")) {
-                ViewDoctorFrequencies.viewMostFrequentDoctor();
+                //ViewDoctorFrequencies.viewMostFrequentDoctor();
             } else {
                 System.out.println("Invalid view frequencies command.");
             }
