98143299763c325fc78ea8533403447061b91d1a
jhu0748 <jackson.j.hu@gmail.com>
2025-04-03 22:23:38 +0800

    Add more assertions

--- src/main/java/seedu/medinote/commands/DeleteDoctor.java
+++ src/main/java/seedu/medinote/commands/DeleteDoctor.java
@@ -1,16 +1,16 @@
 package seedu.medinote.commands;
 
 import java.util.ArrayList;
-import seedu.medinote.person.Doctor;
 import seedu.medinote.manager.DoctorListManager;
+import seedu.medinote.person.Doctor;
 import seedu.medinote.storage.saveData;
 
 public class DeleteDoctor {
-    public static void deleteDoctor(String doctorName) {
-        assert doctorName != null: "Doctor name should not be null.";
-        doctorName = doctorName.trim();
+    public static void deleteDoctor(String docName) {
+        assert docName != null: "Doctor name should not be null.";
+        docName = docName.trim();
 
-        if(doctorName.isEmpty()) {
+        if(docName.isEmpty()) {
             System.out.println("Please provide a doctor name and try again.");
             return;
         }
@@ -20,7 +20,10 @@ public class DeleteDoctor {
         assert (!doctorList.isEmpty()): "Doctor list should not be empty.";
 
         for(Doctor doctor : doctorList) {
-            if(doctor.getName().equalsIgnoreCase(doctorName)) {
+            assert doctor != null: "Doctor object should not be empty.";
+            String currdoctorName = doctor.getName();
+            assert currdoctorName != null : "Doctor name should not be empty.";
+            if(currdoctorName.equalsIgnoreCase(docName)) {
                 DoctorListManager.removeDoctor(doctor);
                 try {
                     saveData.saveDoctorsData(doctorList);
@@ -32,7 +35,7 @@ public class DeleteDoctor {
             }
         }
 
-        System.out.println("Doctor with name \"" + doctorName + "\" was not found");
+        System.out.println("Doctor with name \"" + docName + "\" was not found");
 
     }
 
