6e4db0eff555a5d3d2ef03045fa505a15b71db37
jhu0748 <hu823@purdue.edu>
2025-04-03 22:29:20 +0800

    Update PatientListManager.java

--- src/main/java/seedu/medinote/manager/PatientListManager.java
+++ src/main/java/seedu/medinote/manager/PatientListManager.java
@@ -29,7 +29,8 @@ public class PatientListManager {
             if(patient.getDoctorAssigned().equalsIgnoreCase(doctor.getName())) {
                 assert doctor.getName() != null : "Doctor name should not be empty/null.";
                 doctor.setCurrentPatient("na"); // remove patient from doctor's attribute
-                assert doctor.getPatientsBeingTreated().equalsIgnoreCase("na") : "Patient was not removed from doctor's info.";
+                assert doctor.getPatientsBeingTreated().equalsIgnoreCase("na") : "Patient was not removed" + 
+                    " from doctor's info.";
             }
         }
     }
